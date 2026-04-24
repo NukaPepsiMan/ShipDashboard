73dd05db1009c0f6ebbb7ac0d9b709d937fae90a
NukaPepsiMan <davidelicone@hotmail.it>
2026-04-24 23:05:24 +0200

    Feat(TrackingEvent): creazione model, controller, factory

---
+++ database/factories/TrackingEventFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\TrackingEvent;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<TrackingEvent>
+ */
+class TrackingEventFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
