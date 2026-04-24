de5984b9d1341868258bca751a949603c2336089
NukaPepsiMan <davidelicone@hotmail.it>
2026-04-24 23:39:23 +0200

    Feat(Shipment): definizione model shipments

--- app/Models/Shipment.php
+++ app/Models/Shipment.php
@@ -9,4 +9,20 @@ class Shipment extends Model
 {
     /** @use HasFactory<\Database\Factories\ShipmentFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'tracking_number',
+        'recipent_name',
+        'address',
+        'weight',
+        'departure_date',
+        'delivery_date',
+        'status'
+    ];
+
+
+    // Una spedizone può avere molti eventi
+    public function trackingEvents() {
+        return $this->hasMany(TrackingEvent::class);
+    }
 }
