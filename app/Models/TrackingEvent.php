09fc2c0e0d31237214f7ad3fd98731505b4d01b2
NukaPepsiMan <davidelicone@hotmail.it>
2026-04-24 23:39:42 +0200

    Feat(TrackingEvent): definizione model tracking_events

--- app/Models/TrackingEvent.php
+++ app/Models/TrackingEvent.php
@@ -9,4 +9,17 @@ class TrackingEvent extends Model
 {
     /** @use HasFactory<\Database\Factories\TrackingEventFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'shipment_id',
+        'location',
+        'description',
+        'status',
+        'event_time'
+    ];
+
+    //un evento appartiene a una spedizione
+    public function shipment() {
+        return $this->belongsTo(Shipment::class);
+    }
 }
