b66fdbd05058028b7b8ac0087fb792b11fcad21c
NukaPepsiMan <davidelicone@hotmail.it>
2026-04-25 14:36:29 +0200

    feat(Api): definizione controller ed index, show endpoints

---
+++ app/Http/Controllers/api/ShipmentApiController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\ShipmentResource;
+use App\Models\Shipment;
+use Illuminate\Http\Request;
+
+class ShipmentApiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return ShipmentResource::collection(Shipment::paginate(10));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Shipment $shipment)
+    {
+        $shipment->load('trackingEvents');
+        return ShipmentResource::make($shipment);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Shipment $shipment)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Shipment $shipment)
+    {
+        //
+    }
+}
