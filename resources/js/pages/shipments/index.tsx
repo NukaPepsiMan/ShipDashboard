987a6d0bc62a6f54e367fc8ecb3f1906421d3029
NukaPepsiMan <davidelicone@hotmail.it>
2026-04-26 15:33:48 +0200

    Feat(Index): aggiunto componentee UI Table

--- resources/js/pages/shipments/index.tsx
+++ resources/js/pages/shipments/index.tsx
@@ -1,4 +1,5 @@
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { Head } from "@inertiajs/react";
 
 
@@ -16,7 +17,32 @@ export default function index() {
                         <CardTitle>Lista Spedizioni</CardTitle>
                     </CardHeader>
                     <CardContent>
-
+                        <Table>
+                            <TableHeader>
+                                <TableRow>
+                                    <TableHead>Tracking number</TableHead>
+                                    <TableHead>Destinatario</TableHead>
+                                    <TableHead>Indrizzo</TableHead>
+                                    <TableHead>Peso</TableHead>
+                                    <TableHead>Data di Partenza</TableHead>
+                                    <TableHead>Data di Consegna</TableHead>
+                                    <TableHead>Stato</TableHead>
+                                    <TableHead className="text-right">Azioni</TableHead>
+                                </TableRow>
+                            </TableHeader>
+                            <TableBody>
+                                <TableRow>
+                                <TableCell>-</TableCell>
+                                <TableCell>-</TableCell>
+                                <TableCell>-</TableCell>
+                                <TableCell>-</TableCell>
+                                <TableCell>-</TableCell>
+                                <TableCell>-</TableCell>
+                                <TableCell>-</TableCell>
+                                <TableCell className="text-right">-</TableCell>
+                                </TableRow>
+                            </TableBody>
+                        </Table>
                     </CardContent>
                     <CardFooter>
 
